7afd5e3ad289a1ff0a7df79c99d8c6c19cb5055b
Flávio Codeço Coelho <fccoelho@gmail.com>
2017-07-14 17:15:09 -0300

    Updated notebooks and python modules

--- Qtrading.py
+++ Qtrading.py
@@ -2,6 +2,8 @@ u"""
 Neural Network based trading algorithm 
 trained with reinforcement learning
 
+Adapted from: https://keon.io/deep-q-learning/
+
 Created on 23/03/16
 by fccoelho
 license: GPL V3 or Later
@@ -10,27 +12,139 @@ license: GPL V3 or Later
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, Adam
+from queue import deque
+import numpy as np
+import random
+import pandas as pd
 
 
-# Input layer, this must encode the current state of the system (a time window to the past(prices, volume, asks and bids)
-# and whether you have a position in the market, volume and price.
+def get_price_history(pair, start, end):
+    """
+    Poloniex API only returns maximum of 300000 trades or 1 year for each pair.
+    :returns:
+    dictionary with one dataframe per pair
+    """
+    print('Downloading {} from {} to {}.'.format(pair, start, end))
+    url = 'https://poloniex.com/public?command=returnTradeHistory&currencyPair={}&start={}&end={}'.format(pair,
+                                                                                                          int(start.timestamp()),
+                                                                                                          int(end.timestamp()))
+    df = pd.read_json(url)
+    df.set_index(['date'], inplace=True)
+    print('fetched {} {} trades.'.format(df.size, pair))
+    df = df.resample('1T').mean()  # resample in windows of 1 minute
 
-model = Sequential()
-model.add(Dense(164, init='lecun_uniform', input_shape=(64,)))
-model.add(Activation('relu'))
-#model.add(Dropout(0.2)) I'm not using dropout, but maybe you wanna give it a try?
+    return df
 
-model.add(Dense(150, init='lecun_uniform'))
-model.add(Activation('relu'))
-#model.add(Dropout(0.2))
+def get_ohlc(pair, start, end):
+    print('Downloading {} from {} to {}.'.format(pair, start, end))
+    url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period=300'.format(pair,
+                                                                                                          int(start.timestamp()),
+                                                                                                          int(end.timestamp()))
+    df = pd.read_json(url)
+    df.set_index(['date'], inplace=True)
+    return df
 
-# 3 output units, for three possible actions: Buy, Sell, Pass
-model.add(Dense(3, init='lecun_uniform'))
-model.add(Activation('linear')) #linear output so we can have range of real-valued outputs
 
-rms = RMSprop()
-model.compile(loss='mse', optimizer=rms)
 
-#Todo: implement main loop
-#TODO: implement function to calculate reward.
+
+class DQNAgent:
+    """
+    Q-Learning agent
+    """
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        # Input layer, this must encode the current state of the system (a time window to the past(prices, volume, asks and bids)
+        # and whether you have a position in the market, volume and price.
+
+        model = Sequential()
+        model.add(Dense(164, init='lecun_uniform', input_dim=self.state_size, activation='relu'))
+        #model.add(Dropout(0.2)) I'm not using dropout, but maybe you wanna give it a try?
+
+        model.add(Dense(150, init='lecun_uniform', activation=relu))
+        #model.add(Dropout(0.2))
+
+        # 3 output units, for three possible actions: Buy, Sell, Pass
+        model.add(Dense(3, init='lecun_uniform', input_dim=self.action_size, activation='softmax'))
+        # Softmax activation returns value between 0 and 1 which can be interpreted as probability of each action
+
+        # rms = RMSprop()
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = reward + self.gamma * \
+                                  np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+
+if __name__ == "__main__":
+    episodes = 200
+    import datetime
+    # Obtain the data
+    end = datetime.datetime.now()
+    start = end - datetime.timedelta(days=364)
+    data = get_price_history("USDT_BTC", start, end)
+    # state = [position(T/F), how much, price, market price, volume]
+    agent = DQNAgent(5, 3)
+
+    # Iterate the game
+    for e in range(episodes):
+
+        # reset state in the beginning of each episode
+        # TODO: add balances here
+        state = np.array((data.iloc[0]['rate'], data.iloc[0]['total']))
+
+        # time_t represents each trade in history
+        for time_t in range(500):
+
+            # Decide action
+            action = agent.act(state)
+
+            # Advance the game to the next frame based on the action.
+            # Reward is 1 for every frame the pole survived
+            next_state, reward, done, _ = env.step(action)
+            next_state = np.reshape(next_state, [1, 4])
+
+            # Remember the previous state, action, reward, and done
+            agent.remember(state, action, reward, next_state, done)
+
+            # make next_state the new current state for the next frame.
+            state = next_state
+
+            # done becomes True when the game ends
+            # ex) The agent drops the pole
+            if done:
+                # print the score and break out of the loop
+                print("episode: {}/{}, score: {}"
+                      .format(e, episodes, time_t))
+                break
+        # train the agent with the experience of the episode
+        agent.replay(32)
+
---
+++ poloniex.py
@@ -0,0 +1,83 @@
+u"""
+Created on 14/07/17
+by fccoelho
+license: GPL V3 or Later
+"""
+
+import pandas as pd
+import datetime
+
+
+def get_full_table(pair, start, end):
+    """
+    Gets at most 300000 raw trades
+    """
+    df = pd.read_json(
+        'https://poloniex.com/public?command=returnTradeHistory&currencyPair={}&start={}&end={}'.format(pair, int(
+            start.timestamp()), int(end.timestamp())))
+    df.set_index(['date'], inplace=True)
+    print('fetched {} {} trades.'.format(df.size, pair))
+    return df
+
+
+def get_price_table(pair, start, end):
+    """
+    Poloniex API only returns maximum of 300000 trades or 1 year for each pair.
+    :returns:
+    dictionary with one dataframe per pair
+    """
+    print('Downloading {} from {} to {}.'.format(pair, start, end))
+
+    df = get_full_table(pair, start, end)
+
+    df = df.resample('1T').mean()  # resample in windows of 1 minute
+    df[pair] = df.rate
+    for cname in df.columns:
+        if cname not in pairs:
+            del df[cname]
+
+    return df
+
+
+def concatenate_series(rates):
+    """
+    :parameters:
+    - rates: dictionary with the pairs dataframes
+    """
+    for k, df in rates.items():  # Solve non-unique indices
+        rates[k] = df.loc[~df.index.duplicated(keep='first')]
+    data = pd.concat(rates, axis=1)
+    data.columns = data.columns.droplevel(0)
+    print(data.columns)
+    data.columns = [name.lower() for name in data.columns]  # convenient to save to PGSQL
+    return data
+
+
+def extend_history(pair, df):
+    """
+    Extends a dataframe with data on a pair with older data.
+    :param pair:
+    :param df:
+    :return:
+    """
+    End = df.index.min()
+    Start = end - datetime.timedelta(days=364)
+    dfextra = get_price_table(pair, Start, End)
+    df = df.append(dfextra)  # pd.concat([df,dfextra], axis=0)
+    return df
+
+def get_ohlc(pair, start, end):
+    """
+    Gets OHLC historical data aggregated in 5-minute candlesticks
+    :param pair: Currency pair, e.g. USDT_ETH
+    :param start: unix timestamp
+    :param end: unix timestamp
+    :return: dataframe
+    """
+    print('Downloading {} from {} to {}.'.format(pair, start, end))
+    url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period=300'.format(pair,
+                                                                                                          int(start.timestamp()),
+                                                                                                          int(end.timestamp()))
+    df = pd.read_json(url)
+    df.set_index(['date'], inplace=True)
+    return df
